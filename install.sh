fe1aece9a5257a93beec5f6a00f8f76660c95144
Stuart Whitehead <stuart@springload.co.nz>
2018-02-03 11:02:43 +1300

    Installing oh my zsh, node and yarn

--- install.sh
+++ install.sh
@@ -18,4 +18,38 @@ if exists "zsh"; then
 else
   echo "installing" 
   brew install zsh
+  ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
 fi
+
+echo -ne "Checking for Oh My Zsh… "
+if [ -d $HOME/.oh-my-zsh/ ]; then
+ echo "installed"
+else
+  echo "installing"
+  if exists "curl"; then
+    curl -L http://install.ohmyz.sh | sh
+  else
+    echo "curl is needed to install Oh My Zsh"
+  fi
+fi
+
+echo -ne "Checking for system Node.js… "
+if exists "npm"; then
+ echo "installed"
+else
+  echo "installing"
+  brew install node
+fi
+
+
+echo -ne "Checking for yarn… "
+if exists "yarn"; then
+ echo "installed"
+else
+  echo "installing"
+  brew install yarn
+fi
+
+# Link things up
+echo "Linking things up"
+ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig

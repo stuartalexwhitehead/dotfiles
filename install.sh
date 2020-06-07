aba81bee01170f6498715b9537ef56746075c886
Stuart Whitehead <stuart@springload.co.nz>
2020-06-07 16:27:11 +1200

    Updating Homebrew install script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if exists "brew"; then
  echo "installed"
 else
   echo "installing"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo -ne "Checking for zsh… "
